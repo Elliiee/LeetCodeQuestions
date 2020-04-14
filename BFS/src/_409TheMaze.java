b26762d080dceba64f2f55a3fd50dec1b0c215aa
Elliiee <danyiluozhang@gmail.com>
2020-04-14 10:52:43 -0500

    create project

---
+++ BFS/src/_409TheMaze.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _409TheMaze {
+    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    /*public boolean hasPath(int[][] maze, int[] start, int[] destination){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(start);
+    }
+    */
+
+}
